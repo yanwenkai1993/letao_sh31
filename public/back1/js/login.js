4e4c6f7e39d697f3a1860b25011f118119d01681
wenkaiyan <578865954@.qq.com>
2018-11-26 22:08:40 +0800

    表单校验完成

---
+++ public/back1/js/login.js
@@ -0,0 +1,38 @@
+$(function () {
+
+  $('#form').bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    //配置校验字符
+    fields: {
+      username: {
+        validators: {
+          notEmpty: {
+            message: "用户名不能为空"
+          },
+          stringLength: {
+            min: 2,
+            max: 12,
+            message: "用户名长度必须6-12位"
+          }
+        }
+      },
+      password: {
+        validators: {
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          stringLength: {
+            min: 6,
+            max: 20,
+            message: '密码长度必须6-12位'
+          }
+        }
+      }
+    }
+  })
+
+})
